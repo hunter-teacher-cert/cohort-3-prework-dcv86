53c28b97c4e852e600ee1f07db751d024de40a43
DCV86 <97967740+dcv86@users.noreply.github.com>
2022-06-16 13:13:13 +0000

    Done

--- pre04/Beer.java
+++ pre04/Beer.java
@@ -1,18 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-public class happyHour{
-int beers = 99;
-}
-public static void main(int, beers) {
-    if (n >= 0) {
-        System.out.println("Blastoff!");
-    } else {
-        System.out.println(beers+" bottles of beer on the wall. "+beers + "bottles of beer. ");
-				System.out.println("Take one down, pass it around,")
-				beers = beers - 1;
-				System.out.println(beers+" bottles of beer on the wall!");
-}
-}
+public class Beer {
+	public static void main(String[] args){
+		int beers = 99;
+		String BoB = " bottles of beer on the wall";
+		String ToD = "Take one down, pass it around,";
+		
+		while (beers > 0){
+			System.out.println(beers + BoB +". "+ beers + BoB + ". ");
+			System.out.println(ToD);
+			beers = beers - 1; 
+			System.out.println(beers + BoB + ".");
+		}
 
+		
+	}
 
+	
+}
+ 
